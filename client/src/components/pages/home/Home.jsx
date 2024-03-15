0c679e4508563f2c1cdb566b0e5397625e41f2bf
lhingril <sa.kurschi@gmail.com>
2024-03-15 20:23:15 +0100

    new

---
+++ client/src/components/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../../header/Header"
+import Posts from "../../posts/Posts"
+import Sidebar from "../../sidebar/Sidebar"
+import "./home.css"
+import HardcodedPosts from "../../posts/HardcodedPosts"
+import SumBlog from "../../header/SumBlog"
+
+export default function Home() {
+  return (
+    <div className="header">
+        <Header/>
+        <SumBlog/>
+        <div className="home">
+          <HardcodedPost
