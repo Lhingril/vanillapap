f88cdd9eb4bad2d9b5d2cf49404cda7fd4f29e23
lhingril <sa.kurschi@gmail.com>
2024-03-15 20:33:36 +0100

    better

--- client/src/components/header/SumBlog.jsx
+++ client/src/components/header/SumBlog.jsx
@@ -3,8 +3,8 @@ import "./sumblog.css"
 export default function SumBlog() {
   return (
     <div className="sumblog">
-        <h1>Willkommen bei Pick a Petal - unser Zimmerpflanzen-Blog!</h1>
-      <p> In diesem digitalen Paradies tauchen wir ein in die faszinierende Welt der Zimmerpflanzen, die nicht nur unsere Räume verschönern, sondern auch unser Wohlbefinden steigern. Als Teil eines spannenden Uniprojektes zur Gestaltung einer Website rund um das Thema Zimmerpflanzen, laden wir dich ein, gemeinsam mit uns die Vielfalt, Pflegehinweise und die positiven Auswirkungen von Zimmerpflanzen zu entdecken. Tauche ein in die Welt des Grüns, lass dich inspirieren und erfahre alles Wissenswerte über diese grünen Mitbewohner, die unsere Wohnräume in wahre Wohlfühloasen verwandeln können. Freue dich auf informative Beiträge, praktische Tipps und jede Menge Inspiration für deine persönliche Pflanzensammlung! </p>
+        <h1 className="sumblogH">Willkommen bei Pick a Petal - unser Zimmerpflanzen-Blog!</h1>
+      <p className="sumblogP"> In diesem digitalen Paradies tauchen wir ein in die faszinierende Welt der Zimmerpflanzen, die nicht nur unsere Räume verschönern, sondern auch unser Wohlbefinden steigern. Als Teil eines spannenden Uniprojektes zur Gestaltung einer Website rund um das Thema Zimmerpflanzen, laden wir dich ein, gemeinsam mit uns die Vielfalt, Pflegehinweise und die positiven Auswirkungen von Zimmerpflanzen zu entdecken. Tauche ein in die Welt des Grüns, lass dich inspirieren und erfahre alles Wissenswerte über diese grünen Mitbewohner, die unsere Wohnräume in wahre Wohlfühloasen verwandeln können. Freue dich auf informative Beiträge, praktische Tipps und jede Menge Inspiration für deine persönliche Pflanzensammlung! </p>
     </div>
   )
 }
